f88e6ee3578188a35d0721dcd445fea47faf4a08
Luigi MASTROSERIO <luigi.mastroserio@gmail.com>
2018-01-22 22:50:16 +0100

    remove flex

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -15,29 +15,31 @@ class Product extends Component {
 
     render() {
         let index = this.props.index;
-        let myStyle = "product-" + index % 2;
+        let myStyle = "w3-container w3-card product-" + index % 2;
 
         return (
             <li className={myStyle}>
-                <div className="main">
-                    <p className="smallText">{this.props.product.producer}</p>
-                    <h2>{this.props.product.name} ({this.props.product.pieces})</h2>
-                    {(this.props.product.endDate === "9999-12-31") ?
-                        <p className="smallText">valid du: {this.props.product.startDate} </p> :
-                        <p className="smallText">valid du: {this.props.product.startDate} au: {this.props.product.endDate}</p>
-                    }
-                    <p className="smallText"><i className="red">lun.</i> mar. mer. jeu. ven. <i className="red">sam.</i> <i className="red">dim.</i> </p>
-                </div>
+                <div className="w3-row">
+                    <div className="w3-col m8 l8">
+                        <p className="smallText">{this.props.product.producer}</p>
+                        <h2>{this.props.product.name} ({this.props.product.pieces})</h2>
+                        {(this.props.product.endDate === "9999-12-31") ?
+                            <p className="smallText">valid du: {this.props.product.startDate} </p> :
+                            <p className="smallText">valid du: {this.props.product.startDate} au: {this.props.product.endDate}</p>
+                        }
+                        <p className="smallText"><i className="red">lun.</i> mar. mer. jeu. ven. <i className="red">sam.</i> <i className="red">dim.</i> </p>
 
-                <div className="info">
+                    </div>
+                    <div className="w3-col m4 l4">
+                        <p></p>
+                        <button className="w3-button w3-light-green"
+                                type="submit"
+                                onClick={this.remove}>editer</button>
 
-                    <button className="w3-button w3-light-green"
-                            type="submit"
-                            onClick={this.remove}>editer</button>
-
-                    <button className="w3-button w3-black"
-                            type="submit"
-                            onClick={this.remove}>effacer</button>
+                        <button className="w3-button w3-black"
+                                type="submit"
+                                onClick={this.remove}>effacer</button>
+                    </div>
                 </div>
             </li>
         )
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import Product from './Product3W.jsx';
+import Product from './Product.jsx';
 
 
 class ProductList extends Component {
